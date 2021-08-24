e9c31badf5d6dc17e6fc0bb6564ffc7fd415b6cc
jsalbert <albertjimenezsanfiz@gmail.com>
2021-08-24 03:53:47 +0200

    s

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     version='0.0.1',
     description='BioTorch is a PyTorch framework specialized in biologically plausible learning algorithms.',
     author='Albert Jimenez',
-    author_email='',
+    author_email='albertjimenez@aip.ai',
     url='https://github.com/jsalbert/biotorch',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     scripts=[],
     install_requires=[
+        "setuptools"
         "torch>=1.0",
         "torchvision",
         "flake8",
